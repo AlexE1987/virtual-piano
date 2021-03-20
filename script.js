b61e36b467c1ed111a34977ee3f72523f86bfcf8
AlexE1987 <evsikovad1987@gmail.com>
2021-03-20 12:55:28 +0400

    feat: implement fullscreen button

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ btnNotes.addEventListener('click', event => {
 
 // implement function playAudio() and add listeners on mouse for piano keys  
 
-function playAudio(src) {
+const playAudio = (src) => {
     const audio = new Audio();
     audio.src = src;
     audio.currentTime = 0;
@@ -54,38 +54,39 @@ const stopSound = (event) => {
 }
 
 const startCorrespondOver = (event) => {
-    const note = event.target.dataset.note;
-    const src = `assets/audio/${note}.mp3`;
-    playAudio(src);
+        if (event.target.classList.contains('piano-key')) {
+        const note = event.target.dataset.note;
+        const src = `assets/audio/${note}.mp3`;
+        playAudio(src);
 
-    event.target.classList.remove('piano-key-remove-mouse');
-    event.target.classList.add('piano-key-active');
-    event.target.classList.add('piano-key-active-pseudo');
+        event.target.classList.remove('piano-key-remove-mouse');
+        event.target.classList.add('piano-key-active');
+        event.target.classList.add('piano-key-active-pseudo');
 
-    pianoKeys.forEach((elem) => {
-        elem.addEventListener('mouseover', startSound);
-    });
-
-    pianoKeys.forEach((elem) => {
-        elem.addEventListener('mouseout',stopSound);
-    });
+        pianoKeys.forEach((elem) => {
+            elem.addEventListener('mouseover', startSound);
+            elem.addEventListener('mouseout', stopSound);
+        });
+        }
+         
 };
 
-const stopCorrespondOver = () => {
-    pianoKeys.forEach((elem) => {
+const stopCorrespondOver = (event) => {              
+        pianoKeys.forEach((elem) => {
         elem.classList.add('piano-key-remove-mouse');
         elem.classList.remove('piano-key-active');
-        elem.classList.remove('piano-key-active-pseudo');
+        elem.classList.remove('piano-key-active-pseudo');        
         elem.removeEventListener('mouseover', startSound);
-    });
-
-    pianoKeys.forEach((elem) => {
         elem.removeEventListener('mouseout', stopSound);
     });
+        if (event.target.classList.contains('main')) {
+            event.target.removeEventListener();
+        }
+       
 };
 
 piano.addEventListener('mousedown', startCorrespondOver);
-piano.addEventListener('mouseup', stopCorrespondOver);
+window.addEventListener('mouseup', stopCorrespondOver);
 
 // Add keybord keys listeners
 
@@ -117,4 +118,19 @@ window.addEventListener('keydown', startSoundKey);
 window.addEventListener('keyup', stopSoundKey);
 
 
+// implement fullscreen button
+
+const fullscreenBtn = document.querySelector('.fullscreen');
+
+const toggleFullscreen = () => {
+    if(!document.fullscreenElement) {
+        document.documentElement.requestFullscreen();
+    } else {
+        if(document.fullscreenEnabled) {
+            document.exitFullscreen();
+        }
+    };
+};
+
+fullscreenBtn.addEventListener('click', toggleFullscreen);
 
